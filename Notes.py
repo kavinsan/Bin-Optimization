874f969dac5696ca95228128a404b6f0fbd5efc0
kavinsan <kavinsan.thavanesan@ryerson.ca>
2018-07-27 23:26:09 -0400

    Optimized canvas and Notes class

---
+++ Notes.py
@@ -0,0 +1,171 @@
+import tkinter as tk
+import time
+
+class Notes():
+    instances = []
+    label = []
+    counter = 0;
+    
+    def __init__(self, canvas, x0, x1, y0, y1,color):
+        self.canvas = canvas
+        
+        self.x0 = x0
+        self.x1 = x1
+        self.y0 = y0
+        self.y1 = y1
+        self.color = color
+        
+        self.minimum = 400
+        self.maximum = 40000
+        
+        if(not self.create()):
+
+            del self
+        
+    @property
+    def _x0(self):
+        return self.x0
+    
+    @_x0.setter
+    def _x0(self,value):
+        self.x0 = value
+
+    @property
+    def _x1(self):
+        return self.x1
+    
+    @_x1.setter
+    def _x1(self,value):
+        self.x1 = value
+
+    @property
+    def _y0(self):
+        return self.y0
+    
+    @_y0.setter
+    def _y0(self,value):
+        self.y0 = value
+
+    @property
+    def _y1(self):
+        return self.y1
+    
+    @_y1.setter
+    def _y1(self,value):
+        self.y1 = value
+
+    @property
+    def _color(self):
+        return self.color
+    
+    @_color.setter
+    def _color(self,value):
+        self.color = value
+        
+    def _minimum(self,value):
+        self.minimum = value
+
+    def _maximum(self,value):
+        self.maximum = value    
+                        
+    def create(self):
+        #self.refractor()
+        
+        if(not self.border(self.area(),self.minimum,self.maximum)):
+            print("Improper size")
+            return False
+        
+        if(not self.collision(self.x0, self.x1, self.y0, self.y1, self.instances)):
+            print("A rectangle already exists here")
+            return False;
+
+        Notes.counter = Notes.counter + 1;
+        self.id = Notes.counter;
+                
+        self.data = self.canvas.create_rectangle(self.x0,self.y0,self.x1,self.y1, fill=self.color)
+        self.label = self.canvas.create_text(((self.x0 + self.x1)/2, (self.y0 + self.y1)/2), text=self.id)
+        
+        self.instances.append(self) #Add instance to record
+        
+        return True;
+            
+    def refractor(self):
+        if(self.x1 < self.x0):
+            tempX = self.x0
+            self.x0 = self.x1
+            self.x1 = tempX
+        if(self.y1 < self.y0):
+            tempY = self.y0
+            self.y0 = self.y1
+            self.y1 = tempY
+    
+    def area(self):
+        length = self.x1 - self.x0
+        width = self.y1 - self.y0
+        return length * width
+    
+    def border(self, area, minimum, maximum):
+        if not (minimum < (area) < maximum):
+            return False;
+        
+        return True;
+    
+    def collision(self, x0, x1, y0, y1, rectangles):
+        
+        for rects in rectangles:
+            rX0 = rects.x0
+            rX1 = rects.x1
+            rY0 = rects.y0
+            rY1 = rects.y1
+            
+            #Check overlapped corner with respect to the created rectangle and then existing rectangle
+            if(((rX0 <= x0 <= rX1) & (rY0 <= y0 <= rY1)) | ((x0 <= rX0 <= x1) & (y0 <= rY0 <= y1))): #Top Left
+                return False;
+            elif(((rX0 <= x1 <= rX1) & (rY0 <= y1 <= rY1)) | ((x0 <= rX1 <= x1) & (y0 <= rY1 <= y1))): #Bottom Right
+                return False;          
+            elif(((rX0 <= x1 <= rX1) & (rY0 <= y0 <= rY1)) | ((x0 <= rX1 <= x1) & (y0 <= rY0 <= y1))): #Top Right  
+                return False;
+            elif(((rX0 <= x0 <= rX1) & (rY0 <= y1 <= rY1)) | ((x0 <= rX0 <= x1) & (y0 <= rY1 <= y1))): #Bottom Left
+                return False;        
+
+     
+            #over lap without corners with respect to the created rectangle
+            elif(((rX0 <= x0 <= rX1) & (y0 <= rY0 <= y1)) | ((x0 <= rX0 <= x1) & (rY0 <= y0 <= rY1))): #Top Left Intersection   
+                return False; 
+            elif(((rX0 <= x1 <= rX1) & (y0 <= rY0 <= y1)) | ((x0 <= rX1 <= x1) & (rY0  <= y1 <= rY1))): #Top Right Intersection 
+                return False;
+            elif(((rX0 <= x0 <= rX1) & (y0 <= rY1 <= y1)) | ((x0 <= rX0 <= x1) & (rY0  <= y0 <= rY1))): #Bottom Left Intersection  
+                return False;
+            elif(((rX0 <= x1 <= rX1) & (y0 <= rY1 <= y1)) | ((x0 <= rX1 <= x1) & (rY0  <= y1 <= rY1))): #Bottom Right Intersection   
+                return False;
+                         
+        return True; #return of rectangles are not colliding
+    
+    
+    def move(self):
+        yspeed = 2
+        xspeed = 1
+        while(True):
+
+            self.canvas.move(self.data, xspeed, yspeed)
+            self.canvas.move(self.label, xspeed, yspeed)
+            pos=self.canvas.coords(self.data)
+            
+            if(pos[3] >= 400 or pos[1] <= 0):
+                yspeed = -yspeed
+            if(pos[2] >= 1000 or pos[0] <= 0):
+                xspeed = -xspeed
+                            
+            self.canvas.update()
+            self.x0 = pos[0]
+            self.x1 = pos[1]
+            self.y0 = pos[2]
+            self.y1 = pos[3]
+            
+            Notes.instances[self.id - 1].x0 = self.x0
+            Notes.instances[self.id - 1].x1 = self.x1
+            Notes.instances[self.id - 1].y0 = self.y0
+            Notes.instances[self.id - 1].y1 = self.y1
+            
+            time.sleep(0.01)
+        return
--- shapes.py
+++ shapes.py
@@ -1,16 +1,19 @@
 import tkinter as tk
 from random import randint
 import time
+from Notes import *
 
+              
 class square(tk.Tk):
-    rectangles = [];
+
     colors = ["white", "#363636", "crimson", "#CCCCFF","#E3CF57","beige","#6bf98a"]
-    counter = 0;
+
     
     def __init__(self):
         tk.Tk.__init__(self)
+    
         self.x = self.y = 0
-        self.canvas = tk.Canvas(self, width=1000, height=400, cursor="tcross")
+        self.canvas = tk.Canvas(self, width=1000, height=400, cursor="tcross", highlightthickness=0)
         self.canvas.pack(side="top", fill="both", expand=True)
         self.canvas.bind("<Button-1>", self.on_button_press)
         self.canvas.bind("<ButtonRelease-1>", self.on_button_release)
@@ -22,7 +25,9 @@ class square(tk.Tk):
         self.canvas.bind("<BackSpace>", self.deleteAll)
         
         self.canvas.bind('<Motion>', self.position)
-    
+        self.resizable(False, False)
+        
+               
     def position(self, event):
         self.mouseX = event.x
         self.mouseY = event.y
@@ -45,9 +50,7 @@ class square(tk.Tk):
             area = length * width
             if not (400 < (area) < 40000):
                 continue;
-            
-
-            
+    
             #Swap values based on cursor positive-negative position
             if(x1 < x0):
                 tempX = x0
@@ -58,63 +61,65 @@ class square(tk.Tk):
                 y0 = y1
                 y1 = tempY      
                   
-            if not self.create(x0, x1, y0, y1):
+            if Notes(self.canvas,x0,x1,y0,y1,self.colors[randint(0,6)]) == None:
                 continue
             else:
                 break;
         
     def on_button_data(self,event):
         
-        if(len(self.rectangles) == 0):
+        rectangles = Notes.instances
+        
+        if(len(rectangles) == 0):
             print("No Rectangles Exist")
             return
 
         self.x = event.x
         self.y = event.y
         
-        for rects in self.rectangles:
-            if (((self.x > rects[1]) & (self.x < rects[3])) & ((self.y > rects[2]) & (self.y < rects[4]))):
-                
-                print("[ID]: {}, [COLOR]: {}, [AREA]: {}, [x0]: {}, [y0]: {}, [x1]: {}, [y1]: {}".format(self.canvas.itemcget(rects[7], "text"), rects[6], rects[5], rects[1], rects[2], rects[3], rects[4]))   
+        for rects in rectangles:
+            if (((self.x > rects.x0) & (self.x < rects.x1)) & ((self.y > rects.y0) & (self.y < rects.y1))):
+
+                print("[ID]: {}, [COLOR]: {}, [AREA]: {}, [x0]: {}, [y0]: {}, [x1]: {}, [y1]: {}".format(rects.id, rects.color, rects.area(), rects.x0, rects.y0, rects.x1, rects.y1))   
                 return
         
         #If no rectangles are touched then print them all  
-        for rects in self.rectangles:
-            print("[ID]: {}, [COLOR]: {}, [AREA]: {}, [x0]: {}, [y0]: {}, [x1]: {}, [y1]: {}".format(self.canvas.itemcget(rects[7], "text"), rects[6], rects[5], rects[1], rects[2], rects[3], rects[4]))   
+        for rects in rectangles:
+            print("[ID]: {}, [COLOR]: {}, [AREA]: {}, [x0]: {}, [y0]: {}, [x1]: {}, [y1]: {}".format(rects.id, rects.color, rects.area(), rects.x0, rects.y0, rects.x1, rects.y1))   
             
 
     def deleteAll(self,event):
-        for index, rects in enumerate(self.rectangles):
+        for rects in Notes.instances:
 
-            data = rects[0]
-            label = rects[7]
+            data = rects.data
+            label = rects.label
             
-            print("Deleted [ID]: {}, [COLOR]: {}".format(self.canvas.itemcget(rects[7], "text"), rects[6]))
+            print("Deleted [ID]: {}, [COLOR]: {}".format(rects.id, "text"), rects.color)
             self.canvas.delete(data)
             self.canvas.delete(label)
             
-        self.rectangles = []
+        Notes.instances = []
         return
                   
     def on_button_delete(self,event):
         self.x = event.x
         self.y = event.y
-        
-        for index, rects in enumerate(self.rectangles):
-            if(self.x < rects[1]):
+        rectangles = Notes.instances
+        for index, rects in enumerate(rectangles):
+            if(self.x < rects.x0):
                 continue
-            if(self.x > rects[3]):
+            if(self.x > rects.x1):
                 continue
-            if(self.y < rects[2]):
+            if(self.y < rects.y0):
                 continue
-            if(self.y > rects[4]):
+            if(self.y > rects.y1):
                 continue
 
             else:
-                data = rects[0]
-                label = rects[7]
-                self.rectangles = self.rectangles[:index] + self.rectangles[index+1:]
-                print("Deleted [ID]: {}, [COLOR]: {}".format(self.canvas.itemcget(rects[7], "text"), rects[6]))
+                data = rects.data
+                label = rects.label
+                Notes.instances = Notes.instances[:index] + Notes.instances[index+1:]
+                print("Deleted [ID]: {}, [COLOR]: {}".format(rects.id, "text"), rects.color)
                 self.canvas.delete(data)
                 self.canvas.delete(label)
                 return
@@ -122,8 +127,6 @@ class square(tk.Tk):
     def on_button_press(self, event):
         self.x = event.x
         self.y = event.y
-        if(len(self.rectangles) <= 0):
-            return;
              
     def on_button_release(self, event):
         x0,y0 = (self.x, self.y)
@@ -141,101 +144,11 @@ class square(tk.Tk):
             tempY = y0
             y0 = y1
             y1 = tempY
-        
-        #Limit the square size minimum and/or maximum
-  
-        self.create(x0,x1,y0,y1)
-        
-    def move(self, data, label, id, x0, y0, x1, y1):
-        yspeed = 2
-        xspeed = 1
-        while(True):
 
-            self.canvas.move(data, xspeed, yspeed)
-            self.canvas.move(label, xspeed, yspeed)
-            pos=self.canvas.coords(data)
-            
-            if(pos[3] >= 400 or pos[1] <= 0):
-                yspeed = -yspeed
-            if(pos[2] >= 1000 or pos[0] <= 0):
-                xspeed = -xspeed
-                            
-            self.canvas.update()
-            self.rectangles[id][1] = pos[0]
-            self.rectangles[id][2] = pos[1]
-            self.rectangles[id][3] = pos[2]
-            self.rectangles[id][4] = pos[3]
-    
-
-            time.sleep(0.01)
-        return
-    def border(self, area, minimum, maximum):
-        if not (minimum < (area) < maximum):
-            return False;
         
-        return True;
-    
-    def area(self, x0, x1, y0, y1):
-        length = x1 - x0
-        width = y1 - y0
-        return length * width
+        Notes(self.canvas,x0,x1,y0,y1,self.colors[randint(0,6)])
         
-    def create(self, x0, x1, y0, y1):
-        
-        area = self.area(x0,x1,y0,y1)
-        
-        if(not self.border(area, 400,40000)):
-            return False
-        
-        if(not self.collision(x0, x1, y0, y1)):
-            print("A rectangle already exists here")
-            return False;
-        
-        #Assign a color  
-        newColor = self.colors[randint(0,6)]
-        #Create the rectangle
-        data = self.canvas.create_rectangle(x0,y0,x1,y1, fill=newColor)
-        #Assign a label
-        self.counter = self.counter + 1
-        text = "[" + str(self.counter) + "]"
-        label = self.canvas.create_text(((x0 + x1)/2, (y0+y1)/2), text=text)       
-        #Register the rectangle to our array to keep track for events
-        #x0 = 1, y0 = 2, x1 = 3, y1 = 4
-        self.rectangles.append([data, int(self.canvas.coords(data)[0]),int(self.canvas.coords(data)[1]),x1,y1, area,newColor,label])       
-        #self.move(data,label, self.counter - 1, x0,y0,x1,y1)
-        return True;
-    
-    def collision(self, x0, x1, y0, y1):
-        
-        for rects in self.rectangles:
-            rX0 = rects[1]
-            rX1 = rects[3]
-            rY0 = rects[2]
-            rY1 = rects[4]
-            
-            #Check overlapped corner with respect to the created rectangle and then existing rectangle
-            if(((rX0 <= x0 <= rX1) & (rY0 <= y0 <= rY1)) | ((x0 <= rX0 <= x1) & (y0 <= rY0 <= y1))): #Top Left
-                return False;
-            elif(((rX0 <= x1 <= rX1) & (rY0 <= y1 <= rY1)) | ((x0 <= rX1 <= x1) & (y0 <= rY1 <= y1))): #Bottom Right
-                return False;          
-            elif(((rX0 <= x1 <= rX1) & (rY0 <= y0 <= rY1)) | ((x0 <= rX1 <= x1) & (y0 <= rY0 <= y1))): #Top Right  
-                return False;
-            elif(((rX0 <= x0 <= rX1) & (rY0 <= y1 <= rY1)) | ((x0 <= rX0 <= x1) & (y0 <= rY1 <= y1))): #Bottom Left
-                return False;        
-
-     
-            #over lap without corners with respect to the created rectangle
-            elif(((rX0 <= x0 <= rX1) & (y0 <= rY0 <= y1)) | ((x0 <= rX0 <= x1) & (rY0 <= y0 <= rY1))): #Top Left Intersection   
-                return False; 
-            elif(((rX0 <= x1 <= rX1) & (y0 <= rY0 <= y1)) | ((x0 <= rX1 <= x1) & (rY0  <= y1 <= rY1))): #Top Right Intersection 
-                return False;
-            elif(((rX0 <= x0 <= rX1) & (y0 <= rY1 <= y1)) | ((x0 <= rX0 <= x1) & (rY0  <= y0 <= rY1))): #Bottom Left Intersection  
-                return False;
-            elif(((rX0 <= x1 <= rX1) & (y0 <= rY1 <= y1)) | ((x0 <= rX1 <= x1) & (rY0  <= y1 <= rY1))): #Bottom Right Intersection   
-                return False;
-                         
-        return True; #return of rectangles are not colliding
-
 if __name__ == "__main__":
+    
     app = square()
     app.mainloop()
